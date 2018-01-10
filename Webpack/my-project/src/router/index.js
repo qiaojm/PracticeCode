d35d67b7168116cd34a1d2154664df91478c8421
ptspzy <ptspzy@126.com>
2018-01-10 22:33:42 +0800

    feat: 引入组件

--- Webpack/my-project/src/router/index.js
+++ Webpack/my-project/src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import RequestData from '@/pages/RequestData'
+import Page1Container from '@/pages/Page1Container'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Page1Container',
+      component: Page1Container
     },
     {
       path: '/test001',
